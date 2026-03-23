e3a804f6a4d3d25a137938413f9919b892c6f474
Cold <maulik10gajera@gmail.com>
2026-03-23 12:52:38 +0530

    constants

---
+++ src/utils/constants.py
@@ -0,0 +1,8 @@
+MEDAL_WEIGHTS = {"Gold": 3, "Silver": 2, "Bronze": 1}
+VALID_MEDALS  = ["Gold", "Silver", "Bronze"]
+VALID_SEASONS = ["Summer", "Winter"]
+RANDOM_STATE  = 42
+TEST_SIZE     = 0.2
+CV_FOLDS      = 5
+MODEL_DIR     = "models/trained"
+RESULTS_DIR   = "results"
